e172e5ca07719733c01befca8a0ab79ba8ddcc68
Wendell Borges <wendell.borges@grupopermaneo.com.br>
2025-12-23 10:57:12 -0300

    fix: removing facebook icon

--- apps/web/src/lib/content/social.ts
+++ apps/web/src/lib/content/social.ts
@@ -2,26 +2,22 @@ import { socialLinks } from "@/lib/content/portfolio";
 import type { SocialSectionType } from "@/lib/types/sections";
 
 export const socialSection: SocialSectionType = {
-	socialLinks: [
-		{
-			icon: "tabler:brand-github",
-			url: socialLinks.github,
-		},
-		{
-			icon: "mdi:instagram",
-			url: socialLinks.instagram,
-		},
-		{
-			icon: "hugeicons:new-twitter",
-			url: socialLinks.twitter,
-		},
-		{
-			icon: "lucide:linkedin",
-			url: socialLinks.linkedin,
-		},
-		{
-			icon: "lucide:facebook",
-			url: socialLinks.facebook,
-		},
-	],
+  socialLinks: [
+    {
+      icon: "tabler:brand-github",
+      url: socialLinks.github,
+    },
+    {
+      icon: "mdi:instagram",
+      url: socialLinks.instagram,
+    },
+    {
+      icon: "hugeicons:new-twitter",
+      url: socialLinks.twitter,
+    },
+    {
+      icon: "lucide:linkedin",
+      url: socialLinks.linkedin,
+    },
+  ],
 };
